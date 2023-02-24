dc2d0a282de6a6f341ba29fb4de9e8f0c34e820d
Tiffany Han <125551867+tiffanyhansy@users.noreply.github.com>
2023-02-24 22:45:46 +0900

    Create 230223_c언어코딩도장_33.4.c

---
+++ 230223_c언어코딩도장_33.4.c
@@ -0,0 +1,18 @@
+//논리 연산자를 사용하지 않고 3과 5의 공배수 처리하기
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for (int i=0; i<=100; i++)
+    {
+        if(i % 15 == 0) printf("Fizzbuzz\n");
+        else if (i % 3 == 0) printf("Fizz\n");
+        else if (i % 5 == 0) printf("Buzz\n");
+        else printf("%d\n", i);
+    }
+
+    return 0;
+}
