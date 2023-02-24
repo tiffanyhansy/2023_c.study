586da839f6403d3c23919dfba8cf73fb484ccc89
Tiffany Han <125551867+tiffanyhansy@users.noreply.github.com>
2023-02-24 22:41:51 +0900

    Update 230217_c언어코딩도장_30.7.c

--- 230217_c언어코딩도장_30.7.c
+++ 230217_c언어코딩도장_30.7.c
@@ -3,15 +3,29 @@
 
 int main()
 {
-    char c1;
+    int num1;
+    int num2;
+    int i;
 
-    do
+    scanf("%d %d", &num1, &num2);
+    
+    i = num1;
+
+    while (1)
     {
-        scanf("%c", &c1);
+
+        if ( i % 10 == 7)
+        {
+            i++;
+            continue;
+        }
+
+        if (i > num2)
+            break;
+            
+        printf("%d ", i);
+        i++;
     }
 
-    while (c1 != 'q');
-
-    printf("프로그램 종료");
     return 0;
 }
